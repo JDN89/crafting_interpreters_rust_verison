395e7d62a46714c9e8e1f5baf9aba273289134d4
JDN89 <jan.de.niels@gmail.com>
2026-01-22 22:44:47 +0100

    setup token en token type. Nog geen idee wat Object literal zal worden, dus voorlopig gewoon een String van gemakt

--- rlox/src/frontend/token.rs
+++ rlox/src/frontend/token.rs
@@ -1,3 +1,35 @@
+use core::fmt;
+
+
+#[derive(Debug, Clone,  PartialEq, Eq)]
+pub struct Token {
+    ttype: TokenType,
+    lexeme: String,
+    literal: String,
+    line: u32
+}
+
+impl Token {
+    pub fn new (
+        ttype: TokenType,
+        lexeme: String,
+        literal: String,
+        line: u32
+    ) -> Token {
+        Token { ttype, lexeme, literal, line }
+    }
+}
+
+// Optional: implement Display for nicer printing
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Token {{ type: {:?}, lexeme: '{}', literal: '{}', line: {} }}",
+            self.ttype, self.lexeme, self.literal, self.line
+        )
+    }
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum TokenType {
