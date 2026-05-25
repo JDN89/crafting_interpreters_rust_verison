46fcb6356ae51d2ef16e603a4fb4cff40039bebb
JDN89 <jan.de.niels@gmail.com>
2026-05-25 18:38:46 +0200

    Fix test and setup integegration test to fix the ch 9,5 exmaple bug

--- rlox/tests/interpreter_test.rs
+++ rlox/tests/interpreter_test.rs
@@ -1,9 +1,12 @@
-use rlox::{backend::{interpreter::Interpreter, value::LoxValue}, test_eval};
+use rlox::{
+    backend::{interpreter::Interpreter, value::LoxValue},
+    test_eval,
+};
 
 #[test]
 fn test_addition() -> anyhow::Result<()> {
     let input = "1+2;";
-    let result = test_eval(input )?;
+    let result = test_eval(input)?;
 
     assert_eq!(result, LoxValue::Float(3.0));
 
@@ -12,44 +15,59 @@ fn test_addition() -> anyhow::Result<()> {
 
 #[test]
 fn test_while_loop() -> anyhow::Result<()> {
-
-   let input = "
+    let input = "
          var i = 0;
          while (i < 10) {
            print i;
            i = i + 1;
          }
        ";
-   let result = test_eval(input)?;
+    let result = test_eval(input)?;
 
-   assert_eq!(result, LoxValue::Nil);
-   Ok(())
+    assert_eq!(result, LoxValue::Nil);
+    Ok(())
 }
 
 #[test]
 fn test_for_loop() -> anyhow::Result<()> {
-
-   let input = "
+    let input = "
        for (var i = 0; i < 10; i = i + 1) print i;
        ";
-   let result = test_eval(input)?;
+    let result = test_eval(input)?;
 
-   assert_eq!(result, LoxValue::Nil);
-   Ok(())
+    assert_eq!(result, LoxValue::Nil);
+    Ok(())
 }
 #[test]
 fn is_truthy_check() -> anyhow::Result<()> {
-
     let input = r#"
         "hi" or 2;
         nil or "yes";
     "#;
 
+    let result = test_eval(input)?;
 
+    assert_eq!(result, LoxValue::Str("\"yes\"".to_string()));
+
+    Ok(())
+}
+
+#[test]
+fn test_complext_for_loop() -> anyhow::Result<()> {
+    let input = "
+var a = 0;
+var temp;
+
+for (var b = 1; a < 10000; b = temp + b) {
+  print a;
+  temp = a;
+  a = b;
+}
+        ";
 
     let result = test_eval(input)?;
 
-    assert_eq!(result, LoxValue::Str("yes".to_string()));
+    assert_eq!(result, LoxValue::Nil);
 
     Ok(())
 }
