c00c61d83b8951a89c469d67c430d48c3cca416d
JDN89 <jan.de.niels@gmail.com>
2026-05-25 17:15:45 +0200

    refactor lib.rs for easier integration testing

--- rlox/src/lib.rs
+++ rlox/src/lib.rs
@@ -10,25 +10,30 @@ use crate::backend::value::LoxValue;
 use crate::frontend::lexer::Lexer;
 use crate::frontend::parser::Parser;
 
-pub fn run(source: &str, interpreter: &mut Interpreter) -> Result<LoxValue> {
+fn eval_internal(source: &str, interpreter: &mut Interpreter) -> Result<LoxValue> {
     let lexer = Lexer::new(source);
     let tokens = lexer.scan_tokens()?;
-    // for token in &tokens {
-    //     println!("{}", &token);
-    // }
+
     let mut parser = Parser::new(tokens);
-    let statements = parser.parse().context("Parser error: ")?;
-    //
+    let statements = parser.parse()?;
+
     //NOTE ran into a bug where the environemnt inside interpreter got cleared during running of the
     //repl with each new line. the issues was that run is owning and recreating the environment with
     //each function call. solution, make run prompt create the interpreter and pass it here. leave here
     //for now for educational purposes
     // let mut interpreter = Interpreter::new();
-    let result = interpreter
-        .interpret(&statements)
-        .context("Runtime error: ")?;
+    interpreter.interpret(&statements)
+}
+
+pub fn run(source: &str, interpreter: &mut Interpreter) -> Result<LoxValue> {
+    let result = eval_internal(source, interpreter)?;
 
     println!("interpreted result {:?}", result);
 
     Ok(result)
+}
+
+pub fn test_eval(source: &str) -> Result<LoxValue> {
+    let mut interpreter = Interpreter::new();
+    eval_internal(source, &mut interpreter)
 }
--- rlox/tests/interpreter_test.rs
+++ rlox/tests/interpreter_test.rs
@@ -1,19 +1,55 @@
-use rlox::{backend::{interpreter::Interpreter, value::LoxValue}, run};
+use rlox::{backend::{interpreter::Interpreter, value::LoxValue}, test_eval};
 
 #[test]
 fn test_addition() -> anyhow::Result<()> {
-    let mut interpreter = setup_integation_test();
-
     let input = "1+2;";
-    let result = run(input, &mut interpreter)?;
+    let result = test_eval(input )?;
 
     assert_eq!(result, LoxValue::Float(3.0));
 
     Ok(())
 }
 
+#[test]
+fn test_while_loop() -> anyhow::Result<()> {
 
-fn setup_integation_test() -> Interpreter {
-    let interpreter = Interpreter::new();
-    interpreter
+   let input = "
+         var i = 0;
+         while (i < 10) {
+           print i;
+           i = i + 1;
+         }
+       ";
+   let result = test_eval(input)?;
+
+   assert_eq!(result, LoxValue::Nil);
+   Ok(())
+}
+
+#[test]
+fn test_for_loop() -> anyhow::Result<()> {
+
+   let input = "
+       for (var i = 0; i < 10; i = i + 1) print i;
+       ";
+   let result = test_eval(input)?;
+
+   assert_eq!(result, LoxValue::Nil);
+   Ok(())
+}
+#[test]
+fn is_truthy_check() -> anyhow::Result<()> {
+
+    let input = r#"
+        "hi" or 2;
+        nil or "yes";
+    "#;
+
+
+
+    let result = test_eval(input)?;
+
+    assert_eq!(result, LoxValue::Str("yes".to_string()));
+
+    Ok(())
 }
