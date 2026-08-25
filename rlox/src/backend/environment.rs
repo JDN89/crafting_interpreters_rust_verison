5ce3f758f1ed50c8af590d7635187f5218c10b2d
Jan De Niels <jan.de.niels@gmail.com>
2026-08-25 21:36:33 +0200

    some cleanup

--- rlox/src/backend/environment.rs
+++ rlox/src/backend/environment.rs
@@ -41,10 +41,6 @@ impl Environment {
         }))
     }
 
-    // does the lifetime have to persist after this?
-    // No, we are in the interpreter. we store the name and value in the environment so as
-    // to keep track of it for the duration of the program. But we don't need to keep it around after
-    // the function returns, so no clone is needed.
     pub fn define(&mut self, name: String, value: LoxValue) {
         self.values.insert(name, value);
     }
